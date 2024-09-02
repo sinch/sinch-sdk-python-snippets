28b03320b7a3fece8b8a4f4b8259b13b35c41680
650elx <bprokop02@gmail.com>
2024-09-02 20:17:50 +0200

    fix: style

--- voice/text_to_speech.py
+++ voice/text_to_speech.py
@@ -7,8 +7,8 @@ sinch_client = SinchClient(
 
 response = sinch_client.voice.callouts.text_to_speech(
     destination={
-        "type":"number",
-        "endpoint":"YOUR_phone_number"
+        "type": "number",
+        "endpoint": "YOUR_phone_number"
     },
     text="Hello, this is a call from Sinch. Congratulations! You made your first call.")
 
